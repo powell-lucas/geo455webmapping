376d340f050eed14de727f5a320b52afd4a8a6a7
powell-lucas <l.pow28@gmail.com>
2026-04-22 19:47:45 -0500

    Add files via upload

---
+++ Lab8/geo455_lab8.js
@@ -0,0 +1,50 @@
+var mymap = L.map('map').setView([43.06729, -89.418123], 8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(mymap);
+
+// Fetching GeoJSON File
+fetch('lab8_data/wi_il_gcs.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(wi_ilData) {
+    var wi_ilLayer = L.geoJSON(wi_ilData, {
+      style: {
+        color: '#444',
+        weight: 1,
+        fillColor: '#d4e6f1',
+        fillOpacity: 0.4
+      },
+      onEachFeature: function(feature, layer) {
+        if (feature.properties && feature.properties["wi_il_gcs.NAME"]) {
+          layer.bindTooltip(feature.properties["wi_il_gcs.NAME"], {
+            permanent: true,
+            direction: 'center',
+            className: 'county-label'
+          });
+        }
+      }
+    }).addTo(mymap);
+
+    // Fit Map to GeoJSON layer
+    mymap.fitBounds(wi_ilLayer.getBounds());
+
+    // Create migration layer AFTER map is fully ready
+    window.commuteLayer = new L.migrationLayer({
+      map: mymap,
+      data: data,
+      pulseRadius: 30,
+      pulseBorderWidth: 3,
+      arcWidth: 1,
+      arcLabel: true,
+      arcLabelFont: '10px sans-serif',
+      maxWidth: 10
+    });
+
+    commuteLayer.addTo(mymap);
+  })
+  .catch(function(error) {
+    console.error('Error loading GeoJSON file:', error);
+  });
